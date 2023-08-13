b143ff1848a87492a6b49c2b2b3b9b0879d87cf1
Vinu <kevinduss1@gmail.com>
2023-08-13 13:31:42 +0000

    refactor navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
+type NavigationItem = {
+  routerLink: string;
+  symbol: string;
+};
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  navigationItems: NavigationItem[] = [
+    { routerLink: 'qr-generator', symbol: 'qr_code' },
+    { routerLink: 'qr-scanner', symbol: 'barcode_reader' },
+    { routerLink: 'home', symbol: 'home' },
+    { routerLink: 'profile', symbol: 'account_circle' },
+  ];
+}
