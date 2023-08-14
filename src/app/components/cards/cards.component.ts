3fee22d4d30bd8811e27e6eb59c399504fc8b6f5
Vinu <kevinduss1@gmail.com>
2023-08-14 08:34:45 +0000

    test connection to be

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+type Cards = {
+  count?: number;
+};
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
 })
-export class CardsComponent {}
+export class CardsComponent implements OnInit {
+  cards$!: Observable<Cards>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getCards();
+  }
+
+  getCards() {
+    this.cards$ = this.http.get<Cards>(
+      'https://g-collection.azurewebsites.net/cards',
+    );
+  }
+}
