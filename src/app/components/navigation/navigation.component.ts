885ac11d9bad2bb0772dc371ec8c82d586fb66d4
Vinu <kevinduss1@gmail.com>
2023-08-27 07:12:16 +0000

    switch scanner symbol

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -12,7 +12,7 @@ type NavigationItem = {
 })
 export class NavigationComponent {
   navigationItems: NavigationItem[] = [
-    { routerLink: 'qr-scanner', symbol: 'barcode_reader', text: 'Scanner' },
+    { routerLink: 'qr-scanner', symbol: 'qr_code_scanner', text: 'Scanner' },
     { routerLink: 'home', symbol: 'home', text: 'Home' },
     { routerLink: 'cards', symbol: 'diversity_1', text: 'Chärtli' },
   ];
