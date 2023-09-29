2b9d0ca37e9afc76f8f279a600e77dbadb03bcb5
Mathis Dedial <mathis.dedial@ipt.ch>
2023-09-29 16:51:40 +0200

    Remove redundant FormModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -153,7 +153,6 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
       logOnly: environment.production,
     }),
     EffectsModule.forRoot([ProfileEffects, CardEffects]),
-    FormsModule,
   ],
   providers: [
     {
