d62ec46e8a4fdbd9a2c89d2f0bcd61701aef8798
kiliandresse <kiliandresse@aol.com>
2023-10-04 09:32:45 +0200

    Update header to check if a user is admin

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectUser } from '../../state/user/user.selectors';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  user$: Observable<User | null>;
+
+  constructor(private readonly store: Store) {
+    this.user$ = this.store.select(selectUser);
+  }
+}
