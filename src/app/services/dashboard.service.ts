26cbcc9e3195fb11f9e354c20af77e2bf94a2440
kiliandresse <kiliandresse@aol.com>
2023-10-05 17:32:41 +0200

    Get rid off profile null check to catch errors instead

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -2,8 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Dashboard, Ranking } from '../models/dashboard.model';
 import { environment } from 'src/environments/environment';
-import { Profile } from '../models/profile.model';
-import { Observable, of, switchMap } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,23 +10,8 @@ import { Observable, of, switchMap } from 'rxjs';
 export class DashboardService {
   constructor(private readonly http: HttpClient) {}
 
-  getDashboardData(user: Observable<Profile | null>): Observable<Dashboard> {
-    return user.pipe(
-      switchMap((user) => {
-        // Return empty dashboard if no user is found
-        if (user === null) {
-          return of<Dashboard>({
-            myCardsCount: 0,
-            myUniqueCardsCount: 0,
-            allCardsCount: 0,
-            duplicateCardsCount: 0,
-            rankingList: [],
-          });
-        }
-
-        return this.http.get<Dashboard>(`${environment.backendUri}/overview`);
-      }),
-    );
+  getDashboardData(): Observable<Dashboard> {
+    return this.http.get<Dashboard>(`${environment.backendUri}/overview`);
   }
 
   getRankOf(userEmail: string, rankingList: Ranking[]): number {
