6fa0df95ad8205923564d4977098415e76dea051
kiliandresse <kiliandresse@aol.com>
2023-10-05 18:36:56 +0200

    dashboard service cleanup

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Dashboard, Ranking } from '../models/dashboard.model';
+import { Dashboard } from '../models/dashboard.model';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
@@ -12,11 +12,5 @@ export class DashboardService {
 
   getDashboardData(): Observable<Dashboard> {
     return this.http.get<Dashboard>(`${environment.backendUri}/overview`);
-  }
-
-  getRankOf(userEmail: string, rankingList: Ranking[]): number {
-    return (
-      rankingList.find((ranking) => ranking.userEmail === userEmail)?.rank ?? 0
-    );
   }
 }
